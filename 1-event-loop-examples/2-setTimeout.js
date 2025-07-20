411f0ef2ab0bec542aef57f269b966881c4d4acc
Javed003fr <javed.ali003fr@gmail.com>
2025-07-21 03:49:14 +0600

    second commit

---
+++ 1-event-loop-examples/2-setTimeout.js
@@ -0,0 +1,7 @@
+// started operation system process
+console.log("first");
+setTimeout(() => {
+  console.log("second");
+}, 0);
+console.log("third");
+// completed and exited(exit) operating system process
---
+++ 1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+  console.log("hello wordl");
+}, 2000);
+console.log("I will run first");
+// process stays alive unless
+// Kill Process CONTROL + C
+// unexpected error
