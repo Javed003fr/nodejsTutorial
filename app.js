513c29d957c6cac7e3036cbbf37a3b7b721dfa42
Javed003fr <javed.ali003fr@gmail.com>
2025-07-26 15:25:26 +0600

    third commit

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-var http = require("http");
-var fs = require("fs");
+const http = require("http");
+const { readFile, readFileSync } = require("fs");
 
-http
-  .createServer(function (req, res) {
-    // const text = fs.readFileSync("./content/big.txt", "utf8");
-    // res.end(text);
-    const fileStream = fs.createReadStream("./content/big.txt", "utf8");
-    fileStream.on("open", () => {
-      fileStream.pipe(res);
-    });
-    fileStream.on("error", (err) => {
-      res.end(err);
-    });
-  })
-  .listen(5000);
+//get all files
+const homePage = readFileSync("./2-express-tutorial/navbar-app/index.html");
+
+const server = http.createServer((req, res) => {
+  // console.log(req.method);
+  const url = req.url;
+  // home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
+  }
+  // about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>content not found!</h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
+
+// next lesson will start from 009 HTTP - HTML FILE
