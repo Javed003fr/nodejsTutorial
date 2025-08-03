39026bb795a9d392f78da9ad141e527e989c5535
Javed003fr <javed.ali003fr@gmail.com>
2025-08-04 01:04:22 +0600

    Create Task-Manager-App

---
+++ 3-Task-Manager/controllers/tasks.js
@@ -0,0 +1,60 @@
+const Task = require("../models/Task");
+const asyncWrapper = require("../middleware/async");
+
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+
+  // res.status(200).json({ tasks });
+  // res.status(200).json({ tasks, amount:tasks.length });
+  res
+    .status(200)
+    .json({ success: true, data: { tasks, nbHits: tasks.length } });
+});
+
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
+
+const getSingleTask = asyncWrapper(async (req, res) => {
+  const { id: taskId } = req.params;
+  const task = await Task.findOne({ _id: taskId });
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id : ${taskId}` });
+  }
+
+  res.status(200).json({ task });
+});
+
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskId } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id : ${taskId}` });
+  }
+
+  res.status(200).json({ task });
+});
+
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskId } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskId });
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id : ${taskId}` });
+  }
+
+  res.status(200).json({ task });
+  // res.status(200).send();
+  // res.status(200).json({ task: null, status: "success" });
+});
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getSingleTask,
+  updateTask,
+  deleteTask,
+};
